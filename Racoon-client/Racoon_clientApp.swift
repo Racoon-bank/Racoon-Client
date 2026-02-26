4ae8babf0bc65bc481918d7650b1c72fd70899de
Dark-Type <SKAZHUTIN.N@MAIL.RU>
2026-02-26 20:47:00 +0700

    Initial commit

---
+++ Racoon-client/Racoon_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  Racoon_clientApp.swift
+//  Racoon-client
+//
+//  Created by dark type on 26.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct Racoon_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
